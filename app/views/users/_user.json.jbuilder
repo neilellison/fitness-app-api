3da3f0895c87176c3338b45860eb25dab02a129c
NeilEllison <ellison.neil1@gmail.com>
2023-05-08 13:15:42 -0700

    add index action

---
+++ app/views/users/_user.json.jbuilder
@@ -0,0 +1,5 @@
+json.id user.id
+json.name user.name
+json.email user.email
+json.password_digest user.password_digest
+json.image_url user.image_url
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,4 +7,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       assert_response 201
     end
   end
+
+  test "index" do
+    get "/users.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal User.count, data.length
+  end
 end
